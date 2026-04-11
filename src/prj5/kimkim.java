960679c75b8a2f2fdea2e071cde1fd27d12584ac
Dohoon Kim <op@kdh.io>
2026-04-11 00:09:52 -0400

    Update kimkim.java

--- src/prj5/kimkim.java
+++ src/prj5/kimkim.java
@@ -10,11 +10,13 @@ package prj5;
 public class kimkim
 {
     //~ Fields ................................................................
+    // Intentional Conflict!
+    private boolean conflict = true;
 
     //~ Constructors ..........................................................
 
     //~Public  Methods ........................................................
     public static void main(String[] args) {
-        System.out.print("hi");
+        System.out.print("Hello, World!");
     }
 }
