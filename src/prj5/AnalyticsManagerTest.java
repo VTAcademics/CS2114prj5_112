5ef4ed0006097726523004047f32b592fede538a
Dohoon Kim <op@kdh.io>
2026-04-23 22:35:11 -0400

    test: Create Stub Test File for AnalyticsManager

---
+++ src/prj5/AnalyticsManagerTest.java
@@ -0,0 +1,19 @@
+package prj5;
+
+// -------------------------------------------------------------------------
+/**
+ * Tests methods in AnalyticsManager class.
+ * 
+ * @author Jai Mylavarupu
+ * @version Apr 23, 2026
+ */
+public class AnalyticsManagerTest
+    extends student.TestCase
+{
+    // ~ Fields ................................................................
+
+    // ~ Constructors ..........................................................
+
+    // ~ Public Methods ........................................................
+
+}
